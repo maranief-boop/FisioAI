de19cc14b359b6ba9a0e37dd8bf0fbd7c6a44c96
maranief-boop <maranief@gmail.com>
2026-07-30 11:52:53 -0300

    Add files via upload

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,11 +1,36 @@
 import { useMemo } from 'react'
 import { marked } from 'marked'
+import katex from 'katex'
+
+const MATH_PATTERN = /\$\$([\s\S]*?)\$\$|\$([^$\n]+?)\$/g
+
+function renderKatex(text) {
+  const blocks = []
+  let idx = 0
+  const processed = text.replace(MATH_PATTERN, (_, block, inline) => {
+    const placeholder = `%%KATEX_${idx}%%`
+    try {
+      const html = katex.renderToString(block || inline, {
+        throwOnError: false,
+        displayMode: !!block
+      })
+      blocks.push(html)
+    } catch {
+      blocks.push(block || inline)
+    }
+    idx++
+    return placeholder
+  })
+
+  const html = marked.parse(processed, { breaks: true, gfm: true })
+  return html.replace(/%%KATEX_(\d+)%%/g, (_, id) => blocks[+id])
+}
 
 export default function ChatMessage({ message }) {
   const isUser = message.role === 'user'
 
   const html = useMemo(() => {
-    return marked.parse(message.text, { breaks: true, gfm: true })
+    return renderKatex(message.text)
   }, [message.text])
 
   return (
