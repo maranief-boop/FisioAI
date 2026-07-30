5dc4e7ae856bfe81f59851b74046b7717a118581
maranief-boop <maranief@gmail.com>
2026-07-30 11:41:27 -0300

    Add files via upload

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const APP_NAME = 'FisioAI'
 export const STORAGE_KEYS = { API_KEY: '@fisioai/api_key', HISTORY: '@fisioai/history' }
-export const GEMINI_MODEL = 'gemini-3.5-flash'
+export const GEMINI_MODEL = 'gemini-3.5-flash-lite'
 export const MAX_HISTORY_LENGTH = 20
